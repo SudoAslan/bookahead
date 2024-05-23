fc363a1d51b20dc163ddee8ed2899d109e814f5a
Diyar Aslan <echoaslan11@gmail.com>
2024-05-23 12:47:24 +0200

    Authentification done

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,27 +1,31 @@
-import express from 'express';
+import express, {Request ,Response } from 'express';
 import { User } from "../model/UserModel";
 
 export const loginRouter = express.Router(); 
 
 // Route für die Benutzeranmeldung
-loginRouter.post('/login', async (req, res) => {
-    console.log("Anfrage an /login Route");
+loginRouter.post('/', async (req: Request, res: Response) => {
+/*     console.log("Anfrage an /login Route");
     const { email, password } = req.body;
-    console.log("EMAIL: " + email + "\n" + "PASSWORD:" + password)
+    console.log("Name: " + name + "\n" + "Password:" + password)
     console.log(req.body)
     try {
-        if (!email || !password) {
+        if (!email  !password) {
             // Überprüfen Sie, ob Anmeldeinformationen fehlen und geben Sie den entsprechenden Statuscode zurück
-            return res.status(400).json({ message: 'E-Mail und Passwort sind erforderlich' });
+            return res.status(400).json({ message: 'Name und Passwort sind erforderlich' });
         }
-        
-        const user = await User.findOne({ email });
-        if (!user || user.password !== password) {
+
+        const user = await User.findOne({  });
+        if (!user  user.password !== password) {
             return res.status(401).json({ message: 'Benutzer nicht gefunden oder ungültige Anmeldeinformationen' });
         }
         res.status(200).json(user);
     } catch (error) {
         res.status(500).json({ message: (error as Error).message });
-    }
+    } */
+        // Hier können Sie nach Bedarf weitere Authentifizierungslogik implementieren
+        // Wenn die Authentifizierung erfolgreich ist, senden Sie den Statuscode 200 zurück
+        res.status(200).json({ message: 'Erfolgreich eingeloggt' });
+
 });
 export default loginRouter;
