de2bffb966bece9328b7d56fe8eaf2b7e74cc47f
Diyar Aslan <echoaslan11@gmail.com>
2024-07-11 02:29:41 +0200

    test7

--- src/index.ts
+++ src/index.ts
@@ -38,6 +38,9 @@ app.use(cors({
 
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static('uploads')); // Serve the uploads directory
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello, Render!');
